234365c3e5098012f3cbaa20b23be62928f09b09
Morgan <quixoticbibliomania@gmail.com>
2017-03-02 09:29:10 -0500

    speedhazardcard for speed limit

---
+++ MilleBornes/src/millebornes/card/SpeedHazardCard.java
@@ -0,0 +1,10 @@
+package millebornes.card;
+
+public class SpeedHazardCard extends HazardCard {
+	private static final long serialVersionUID = 7172182079050455031L;
+
+	public SpeedHazardCard() {
+		super("Speed Limit");
+	}
+
+}
