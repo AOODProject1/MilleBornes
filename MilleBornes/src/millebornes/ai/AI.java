1209dfd38ddd1d243bc5755b7f60be5fc0aa5442
Morgan <quixoticbibliomania@gmail.com>
2017-03-06 10:09:24 -0500

    AI now returns where to put card, DistanceAI improvements

--- MilleBornes/src/millebornes/ai/AI.java
+++ MilleBornes/src/millebornes/ai/AI.java
@@ -5,7 +5,11 @@ import millebornes.card.SafetyCard;
 import millebornes.card.SpeedCard;
 
 public interface AI {
-	public int getBestCard(Card[] hand,
+	/**
+	 * 
+	 * @return {card's index, where to play}
+	 */
+	public int[] getBestCard(Card[] hand,
 			Card compBattle, SafetyCard[] compSafeties, int compDistance, SpeedCard compSpeed,
 			Card playerBattle, SafetyCard[] playerSafeties, int playerDistance, SpeedCard playerSpeed);
 }
--- MilleBornes/src/millebornes/ai/DistanceAI.java
+++ MilleBornes/src/millebornes/ai/DistanceAI.java
@@ -2,25 +2,34 @@ package millebornes.ai;
 
 import millebornes.card.*;
 import millebornes.util.CardName;
+import millebornes.util.Constants;
 import millebornes.util.Countercard;
-
-public class DefaultAI implements AI {
+/**
+ * This AI will only try to go the distance, rather than fight
+ *
+ */
+public class DistanceAI implements AI {
 
 	@Override
-	public int getBestCard(Card[] hand, Card compBattle, SafetyCard[] compSafeties, int compDistance,
+	public int[] getBestCard(Card[] hand, Card compBattle, SafetyCard[] compSafeties, int compDistance,
 			SpeedCard compSpeed, Card playerBattle, SafetyCard[] playerSafeties, int playerDistance,
 			SpeedCard playerSpeed) {
 		Card typeToLookFor = null;
+		int whereToGo=0;
 		if (compBattle instanceof HazardCard) {
 			typeToLookFor = new Countercard((HazardCard)compBattle).getRemedy();
+			whereToGo = Constants.OWNBATTLE;
 		} else if (compBattle instanceof RemedyCard) {
 			if (((RemedyCard)compBattle).equals(new RemedyCard(CardName.ROLL))) {
 				typeToLookFor = new MovementCard(CardName.MILE_200);
+				whereToGo = Constants.OWNDIST;
 			} else {
 				typeToLookFor = new RemedyCard(CardName.ROLL);
+				whereToGo = Constants.OWNBATTLE;
 			}
-		} else {
-			throw new RuntimeException("The card on top of the Battle Pile for the computer is neither a HazardCard nor a RemedyCard");
+		} else { //no cards on battlepile
+			typeToLookFor = new RemedyCard(CardName.ROLL);
+			whereToGo = Constants.OWNDIST;
 		}
 		int bestIndex=0;
 		for (int i=0;i<hand.length;i++) {//find correct card based to typetolookfor
@@ -29,7 +38,7 @@ public class DefaultAI implements AI {
 					break;
 			}
 		}
-		return bestIndex;
+		return new int[] {bestIndex,whereToGo};
 	}
 
 }
