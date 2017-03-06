e5737942df85694e1bea7348692609c1ca53011f
Morgan <quixoticbibliomania@gmail.com>
2017-03-06 09:39:33 -0500

    added ai

--- MilleBornes/src/millebornes/ai/DefaultAI.java
+++ MilleBornes/src/millebornes/ai/DefaultAI.java
@@ -4,31 +4,32 @@ import millebornes.card.*;
 import millebornes.util.CardName;
 import millebornes.util.Countercard;
 
-public class MadRushAI implements AI {
+public class DefaultAI implements AI {
 
 	@Override
 	public int getBestCard(Card[] hand, Card compBattle, SafetyCard[] compSafeties, int compDistance,
 			SpeedCard compSpeed, Card playerBattle, SafetyCard[] playerSafeties, int playerDistance,
 			SpeedCard playerSpeed) {
-		Card bestCard;
 		Card typeToLookFor = null;
-		if (!compBattle.equals(new RemedyCard(CardName.ROLL))) { //change to compbattle isn't a roll card
-			if (compBattle instanceof HazardCard) {
-				typeToLookFor = new Countercard((HazardCard)compBattle).getRemedy();
-			}
+		if (compBattle instanceof HazardCard) {
+			typeToLookFor = new Countercard((HazardCard)compBattle).getRemedy();
 		} else if (compBattle instanceof RemedyCard) {
 			if (((RemedyCard)compBattle).equals(new RemedyCard(CardName.ROLL))) {
 				typeToLookFor = new MovementCard(CardName.MILE_200);
+			} else {
+				typeToLookFor = new RemedyCard(CardName.ROLL);
 			}
 		} else {
-			typeToLookFor = new RemedyCard(CardName.ROLL);
+			throw new RuntimeException("The card on top of the Battle Pile for the computer is neither a HazardCard nor a RemedyCard");
 		}
-		for (int i=0;i<hand.length;i++) {
+		int bestIndex=0;
+		for (int i=0;i<hand.length;i++) {//find correct card based to typetolookfor
 			if (hand[i].getClass().getSimpleName().equals(typeToLookFor.getClass().getSimpleName())) {
-					//find correct card based to typetolookfor
+					bestIndex=i;
+					break;
 			}
 		}
-		return 0;
+		return bestIndex;
 	}
 
 }
