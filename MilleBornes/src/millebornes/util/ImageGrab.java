a34d3c91ed89048433b86fb7c9c0fafc758be314
Morgan <quixoticbibliomania@gmail.com>
2017-03-15 12:58:28 -0400

    ImageGrab only needs to call cards once to load into a Hashmap
    Should end up being faster than calling the cards each time and should
    catch errors earlier

--- MilleBornes/src/millebornes/util/ImageGrab.java
+++ MilleBornes/src/millebornes/util/ImageGrab.java
@@ -3,26 +3,53 @@ package millebornes.util;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
-import millebornes.card.Card;
-
+/**
+ * Loads the images of cards and the back of the card, then returns the card when requested
+ * @author Morgan
+ *
+ */
 public class ImageGrab {
-	public static Image getCardGraphic(Card c) {
+	private static HashMap<CardName, Image> cards = new HashMap<>();
+	private static Image cardBack;
+	/**
+	 * Loads the cards for each CardName into a HashMap for faster processing and less calls to the card files themselves.
+	 * <br><b>THIS MUST BE CALLED BEFORE USING OTHER ImageGrab METHODS!!!!! (Preferably at startup)</b>
+	 */
+	public static void loadCards() {
+		for (CardName c : CardName.values()){
+			try {
+				cards.put(c, ImageIO.read(new File("mbcards/"+c.toString()+".png")));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		try {
-			return ImageIO.read(new File("mbcards/"+c.getName().toString()+".png"));
+			cardBack = ImageIO.read(new File("mbcards/back.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return null;
 	}
+	/**
+	 * Gets the pre-loaded graphic for the card in question.<br>
+	 * Pre: ImageGrab.loadCards() has been called to initialize the card-array
+	 * @param c The CardName of the graphic
+	 * @return the Image corresponding with <b>c</b>. (This still would need to be cast as an Icon to use with JLabel)
+	 */
+	public static Image getCardGraphic(CardName c) {
+		if (cards==null) return null;
+		return cards.get(c);
+	}
+	
+	/**
+	 * Returns the back of the card.<br>
+	 * Pre: ImageGrab.loadCards() has been called to initialize the cardback-image
+	 */
 	public static Image getCardBack() {
-		try {
-			return ImageIO.read(new File("mbcards/back.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		if (cardBack==null) return null;
+		return cardBack;
 	}
 }
