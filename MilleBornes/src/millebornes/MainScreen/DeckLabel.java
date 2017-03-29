192ec522999c5a0e389ac1e0d15373ca0763951d
Morgan <quixoticbibliomania@gmail.com>
2017-03-29 10:09:05 -0400

    DeckLabel get/set

--- MilleBornes/src/millebornes/MainScreen/DeckLabel.java
+++ MilleBornes/src/millebornes/MainScreen/DeckLabel.java
@@ -11,9 +11,18 @@ import millebornes.util.ImageGrab;
 
 public class DeckLabel extends JLabel {
 	ArrayList<Card> deck = new ArrayList<>();
+	public DeckLabel() {
+		super(new ImageIcon(ImageGrab.getCardBack()));
+		deck=null;
+	}
 	public DeckLabel(ArrayList<Card> x){
 		super(new ImageIcon(ImageGrab.getCardBack()));
 		deck = x;
 	}
-	
+	public Card getTopCard() {
+		return deck.remove(0);
+	}
+	public void setDeck(ArrayList<Card> d) {
+		deck=d;
+	}
 }
